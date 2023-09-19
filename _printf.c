e75e4cf2ab3a58b6e435480f4bddddc757fb00ae
Nada Mohamed <naddamuhhamed1@gmail.com>
2023-09-19 15:10:54 -0700

    Revert "fdsgds"
    This reverts commit c82e28fecd3ea6dcd638bd4227bddc7e8b7c3072.

--- _printf.c
+++ _printf.c
@@ -1,44 +1,63 @@
 #include "main.h"
 
-typedef struct {
+/**
+ * struct FormatSpec - Structure to store format specifier information.
+ * @flags: Flags (e.g., '-', '+', '0').
+ * @width: Minimum field width.
+ * @precision: Precision for integers and strings.
+ * @size: Length modifier (e.g., 'h', 'l', 'L').
+ */
+typedef struct FormatSpec
+{
     int flags;
     int width;
     int precision;
     int size;
 } FormatSpec;
 
+/**
+ * print_buffer - Prints the contents of the buffer.
+ * @buffer: Array of characters.
+ * @buff_ind: Index at which to add the next character, represents the length.
+ */
 void print_buffer(char buffer[], int *buff_ind);
-const char *parse_format(const char *p, FormatSpec *spec);
-int handle_format(const FormatSpec *spec, va_list args, char buffer[], int *buff_ind);
 
+/**
+ * _printf - Custom printf function.
+ * @format: Format string.
+ * Return: Number of characters printed.
+ */
 int _printf(const char *format, ...)
 {
+    if (format == NULL)
+        return (-1);
+
     va_list args;
-    const char *p;
+    va_start(args, format);
+
     char buffer[BUFF_SIZE];
     int buff_ind = 0;
     int printed_chars = 0;
 
-    if (format == NULL)
-        return -1;
-
-    va_start(args, format);
-
-    for (p = format; *p; ++p) {
-        if (*p != '%') {
+    for (const char *p = format; *p; ++p)
+    {
+        if (*p != '%')
+        {
             buffer[buff_ind++] = *p;
             if (buff_ind == BUFF_SIZE)
                 print_buffer(buffer, &buff_ind);
             ++printed_chars;
-        } else {
+        }
+        else
+       {
             FormatSpec spec;
             p = parse_format(p, &spec);
             if (!p)
-                return -1;
+                return (-1);
 
-            int printed /* = handle_format(&spec, args, buffer, &buff_ind) */;
+            int printed = handle_format(&spec, args, buffer, &buff_ind);
             if (printed == -1)
-                return -1;
+                return (-1);
             printed_chars += printed;
         }
     }
@@ -46,28 +65,46 @@ int _printf(const char *format, ...)
     print_buffer(buffer, &buff_ind);
     va_end(args);
 
-    return printed_chars;
+    return (printed_chars);
 }
 
+/**
+ * parse_format - Parse the format specifier and update the FormatSpec struct.
+ * @p: Pointer to the format string.
+ * @spec: Pointer to the FormatSpec struct to update.
+ * Return: Pointer to the character after the format specifier.
+ */
 const char *parse_format(const char *p, FormatSpec *spec)
 {
-    /* Implement the logic to parse flags, width, precision, and size */
-    /* Update spec accordingly */
-    /* Return the pointer after the format specifier */
-    return p; /* Replace with your implementation */
+    // Implement the logic to parse flags, width, precision, and size
+    // Update spec accordingly
+    // Return the pointer after the format specifier
 }
 
+/**
+ * handle_format - Handle the format specifier and write the formatted content to buffer.
+ * @spec: Pointer to the FormatSpec struct containing format information.
+ * @args: va_list of arguments.
+ * @buffer: Array of characters to write to.
+ * @buff_ind: Pointer to the buffer index.
+ * Return: The number of characters printed.
+ */
 int handle_format(const FormatSpec *spec, va_list args, char buffer[], int *buff_ind)
 {
-    /* Implement logic to handle the format specifier */
-    /* Write the formatted content to buffer and update buff_ind */
-    /* Return the number of characters printed */
-    return 0; /* Replace with your implementation */
+    // Implement logic to handle the format specifier
+    // Write the formatted content to buffer and update buff_ind
+    // Return the number of characters printed
 }
 
+/**
+ * print_buffer - Prints the contents of the buffer if it exists.
+ * @buffer: Array of characters.
+ * @buff_ind: Pointer to the buffer index, represents the length.
+ */
 void print_buffer(char buffer[], int *buff_ind)
 {
-    if (*buff_ind > 0) {
+    if (*buff_ind > 0)
+    {
         write(1, buffer, *buff_ind);
         *buff_ind = 0;
     }
